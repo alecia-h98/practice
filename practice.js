81c9e5edcc54d9ebd951a4eb652277def7ab7161
Alecia Hagen <alecia.h98@gmail.com>
2024-12-11 16:06:04 -0600

    updates.1

---
+++ practice.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+console.log('The world has blessed me with another day and I am not prepared.');
+
+const favoriteAnimals = [];
+
+function animals(){
+
+}
+
+
+let favoriteCat = {
+    name: 'Jynxies',
+    Characteristics: 'Needy',
+    CelebrityLookAlike: 'Stewie Griffin'
+}
+
+console.log(`This is my boy: ${favoriteCat}`);
