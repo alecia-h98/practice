aeea74eb4147c8724805a8aab683a924152a5c3d
Alecia Hagen <alecia.h98@gmail.com>
2025-01-05 13:41:08 -0600

    basic variable math

--- practice.js
+++ practice.js
@@ -1,17 +1,31 @@
 console.log('Hello world');
 console.log('The world has blessed me with another day and I am not prepared.');
 
+//basic variable math
+let a = 10
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+//math
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+//this is an open/empty array
 const favoriteAnimals = [];
 
+//this is a function
 function animals(){
+
 
 }
 
-
+//
 let favoriteCat = {
     name: 'Jynxies',
-    Characteristics: 'Needy',
-    CelebrityLookAlike: 'Stewie Griffin'
+    characteristics: 'Needy',
+    celebrityLookAlike: 'Stewie Griffin'
 }
 
 console.log(`This is my boy: ${favoriteCat}`);
